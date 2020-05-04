6e512f56c2074524f227b522173e4ac7f93b642c
kelxh <kelmentdjelili@gmail.com>
2020-05-04 13:53:50 +0200

    part3

---
+++ part3/phonebook-fullstack/phonebook-backend/build/precache-manifest.5201c5603f92af6975d341c03aea1f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3b7da4aba090d54d1ebc0f4c44a673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4510fd3afbd5c8d9fcb8",
+    "url": "/static/css/main.af0d7be9.chunk.css"
+  },
+  {
+    "revision": "3f9ab6002d1763035254",
+    "url": "/static/js/2.d2f57b5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2f57b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4510fd3afbd5c8d9fcb8",
+    "url": "/static/js/main.bdbea220.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
