f8622010d6bf610816eaefd2c919432e9029da46
Kelment <kelmentdjelili@gmail.com>
2022-03-23 13:04:20 +0100

    9.1 Body mass index

---
+++ part9/first_steps_with_typescript/calculateBmi.ts
@@ -0,0 +1,22 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmiResult: number = weight / Math.pow(height / 100, 2);
+    if (bmiResult < 16) {
+      return "Underweight (Severe thinness)";
+    } else if (bmiResult < 16.9) {
+      return "Underweight (Moderate thinness)";
+    } else if (bmiResult < 18.4) {
+      return "Underweight (Mild thinness)";
+    } else if (bmiResult < 24.9) {
+      return "Normal (healthy weight)";
+    } else if (bmiResult < 29.9) {
+      return "Overweight (Pre-obese)";
+    } else if (bmiResult < 34.9) {
+      return "Obese (Class I)";
+    } else if (bmiResult < 39.9) {
+      return "Obese (Class II)";
+    } else {
+      return "Obese (Class III)";
+    }
+  };
+
+  console.log(calculateBmi(180, 74))
