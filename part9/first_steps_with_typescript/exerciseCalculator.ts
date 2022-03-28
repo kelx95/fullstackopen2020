7189003861da8df40e8c901f37222631dd45df33
Kelment <kelmentdjelili@gmail.com>
2022-03-28 19:45:29 +0200

    9.6 Eslint & 9.7 WebExercises

--- part9/first_steps_with_typescript/exerciseCalculator.ts
+++ part9/first_steps_with_typescript/exerciseCalculator.ts
@@ -7,6 +7,16 @@ interface ReviewResult {
   target: number;
   average: number;
 }
+
+interface Request {
+  daily_exercises?: number[],
+  target?: number
+}
+
+interface ErrorObject {
+  error: string;
+}
+
 const calculateExercises = (
   exercises: number[],
   target: number
@@ -26,12 +36,11 @@ const calculateExercises = (
       rating = 3;
       ratingDescription = 'Great work!';
   } else if (target - average < 0.5) {
-      console.log(target - average);
       rating = 2;
       ratingDescription = 'not too bad but could be better';
   } else {
       rating = 1;
-      ratingDescription = 'Not bad, but you can do better';
+      ratingDescription = 'bad';
   }
 
   return {
@@ -59,9 +68,41 @@ const parseInputCalculateExercises = (
     }
     return calculateExercises(exercises, target);
   };
+
   if (process.argv.length > 2) {
     const [, , target, ...exercises] = process.argv;
     console.log(parseInputCalculateExercises(target, exercises));
   }
+
+const parseRequest = ( requestBody: Request )  => {
+  const daily_exercises = requestBody?.daily_exercises??[];
+  const target = requestBody?.target;
+
+  if (!target || daily_exercises?.length === 0) {
+    throw new Error("parameters missing");
+  }
+  if (isNaN(target) || daily_exercises.some((e) => {
+    if(typeof e === "string") return true;
+    return isNaN(e);
+  })) {
+    throw new Error("malformatted parameters");
+  }
+  return {
+    daily_exercises,
+    target
+  };
+};
+
+export const exerciseCalculator = (request: Request): ReviewResult | ErrorObject => {
+  try {
+    const { daily_exercises, target } = parseRequest(request);
+    return calculateExercises(daily_exercises, target);
+  } catch (err) {
+    const errorMessage = (err as Error).message;
+    return {
+      error: errorMessage,
+    };
+  }
+};
 
 
--- part9/first_steps_with_typescript/index.ts
+++ part9/first_steps_with_typescript/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { bmiCalculator } from "./calculateBmi";
+import { exerciseCalculator } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -9,6 +11,12 @@ app.get('/hello', (_req, res) => {
 
 app.get('/bmi', (_req, res) => {
   return res.json(bmiCalculator(_req.query));
+});
+
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return res.json(exerciseCalculator(req.body));
 });
 
 const PORT = 3002;
