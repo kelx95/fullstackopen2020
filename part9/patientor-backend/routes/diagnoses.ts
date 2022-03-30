498c6338a326d8703396f1f3cee98388f630e6be
Kelment <kelmentdjelili@gmail.com>
2022-03-30 15:33:27 +0200

    9.10: Patientor backend, step3

---
+++ part9/patientor-backend/routes/diagnoses.ts
@@ -0,0 +1,16 @@
+
+
+import express from 'express';
+import diagnoseService from '../services/diagnoseService';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  res.send(diagnoseService.getAll());
+});
+
+router.post('/', (_req, res) => {
+    res.send('Saving a diagnose!');
+});
+
+export default router;
